631e399b00f6424b1e5af53e6d178d83370d29d9
Nina Kin <matikin9@gmail.com>
2022-12-29 13:17:38 -0800

    show markers with hhandling for missing vehicles and trips

---
+++ docs/js/data2.js
@@ -0,0 +1,29 @@
+import * as Map from './map.js';
+
+const API_URL = 'https://api.metro.net/';
+const AGENCY_ID = 'LACMTA';
+const TRIP_DETAIL_ENDPOINT = API_URL + AGENCY_ID + '/trip_detail/';
+
+const artBusIds = ['3944', '4111', '5621'];
+
+function loadData() {
+    let fetch_positions = [
+        fetch(TRIP_DETAIL_ENDPOINT + artBusIds[0] + '?geojson=false', { method: "GET", headers: { 'Content-Type': 'application/json', 'mode': 'no-cors'}}),
+        fetch(TRIP_DETAIL_ENDPOINT + artBusIds[1] + '?geojson=false', { method: "GET", headers: { 'Content-Type': 'application/json', 'mode': 'no-cors'}}),
+        fetch(TRIP_DETAIL_ENDPOINT + artBusIds[2] + '?geojson=false', { method: "GET", headers: { 'Content-Type': 'application/json', 'mode': 'no-cors'}})
+    ];
+
+    Map.create('map');
+
+    Promise.all(fetch_positions)
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(trip_details => {
+        trip_details.forEach(elem => {
+            Map.addBusMarker(elem[0]);
+        });
+    });
+}
+
+export {
+    loadData as loadData
+};
